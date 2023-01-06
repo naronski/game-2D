b9d38b53c0621a17918263324e9222069242f058
naronski <122081828+naronski@users.noreply.github.com>
2023-01-06 10:44:10 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1672987173,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/верхнийгород.png",
+		"images/кирпичи.png",
+		"images/стекло-sheet0.png",
+		"images/металл-sheet0.png",
+		"images/hero-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/алмаз-sheet0.png",
+		"images/tiledbackground2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
